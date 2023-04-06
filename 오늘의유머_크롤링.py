6dc5e8149f9a6d87219745dd68c383572b46822b
papasmf3000 <papasmf3000@outlook.kr>
2023-04-06 10:37:31 +0900

    웹크롤링연습3

--- 오늘의유머_크롤링.py
+++ 오늘의유머_크롤링.py
@@ -10,10 +10,12 @@ import re
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 #파일에 저장(r: raw string notation)
-f = open(r"c:\work\clien.txt", "wt", encoding="utf-8")
-for n in range(0,10):
+#기존 컨텐츠에 추가(append, read, write: a+)
+f = open(r"c:\work\todayHumor.txt", "a+", encoding="utf-8")
+#현재는 1~10페이지
+for n in range(1,11):
         #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        data = 'https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, headers = hdr)
@@ -21,15 +23,14 @@ for n in range(0,10):
         #한글이 깨지는 경우는 유니코드로 다시 해석 
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
-        # <span class="subject_fixed" data-role="list-title-text" title="아이패드 에어3 10.5인치 64GB LTE+Wi-Fi 팝니다.">
-        # 	아이패드 에어3 10.5인치 64GB LTE+Wi-Fi 팝니다.
-        # </span>
+        list = soup.find_all('td', attrs={'class':'subject'})
+        #<td class="subject">
+        # <a href="/board/view.php?table=bestofbest>우리를 악에서 
         for item in list:
                 try:
-                        title = item.text  
-                        #print(title.strip())
-                        if (re.search('아이패드', title)):
+                        title = item.find('a').text.strip()  
+                        #print(title)
+                        if (re.search('한국', title)):
                                 print(title.strip())
                                 f.write(title.strip() + "\n")
                 except:
